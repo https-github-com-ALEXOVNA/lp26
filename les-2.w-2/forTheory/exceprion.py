41fdfb772d03c9ed9bdf759a0de62580db15ad69
Álexovna <52166127+ALEXOVNA@users.noreply.github.com>
2022-09-11 01:53:46 -0700

    Delete les-2.w-2/forTheory directory

--- les-2.w-2/forTheory/exceprion.py
+++
@@ -1,9 +0,0 @@
-def cut_cake(people):
-    try:
-        z = 1/ people
-        print(f'Everybody will get {z} - part of cake')
-    except (ZeroDivisionError, TypeError):
-        print('ZeroDevisionExceprion or TypeError')
-
-
-cut_cake(people='0')
